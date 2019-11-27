26ceac494e87566ada2f352fd63a5915f98cf2ae
leokaka1 <leocaoxiaozhu@163.com>
2019-11-27 17:06:10 +0800

    用antD去渲染一个列表组件

--- src/Pages/customer.js
+++ src/Pages/customer.js
@@ -1,83 +1,86 @@
 // 有状态组件
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // 引入ant-design-mobile
-import {Button} from'antd-mobile'
+import { Button, List } from "antd-mobile";
 // import 'antd-mobile/dist/antd-mobile.css'
 
 export default class customer extends Component {
-    constructor(props){
-        console.log('constructor执行')
-        super(props)
-        this.state={
-            name:["Leon","sisi","Giva"]
-        }
-        // 在初始化的时候绑定对应的事件
-        this.addOneItem1 = this.addOneItem1.bind(this)
-    }
-    addOneItem(){
-        this.setState({
-            name:[...this.state.name,"shasha"]
-        })
-    }
+  constructor(props) {
+    console.log("constructor执行");
+    super(props);
+    this.state = {
+      name: ["Leon", "sisi", "Giva"]
+    };
+    // 在初始化的时候绑定对应的事件
+    this.addOneItem1 = this.addOneItem1.bind(this);
+  }
+  addOneItem() {
+    this.setState({
+      name: [...this.state.name, "shasha"]
+    });
+  }
 
-    addOneItem1(){
-        this.setState({
-            name:[...this.state.name,"shasha"]
-        })
-    }
+  addOneItem1() {
+    this.setState({
+      name: [...this.state.name, "shasha"]
+    });
+  }
 
-    componentWillMount(){
-        console.log('componentWillMount执行')
-    }
+  componentWillMount() {
+    console.log("componentWillMount执行");
+  }
 
-    componentDidMount(){
-        console.log('componentDidMount执行')
-    }
+  componentDidMount() {
+    console.log("componentDidMount执行");
+  }
 
-    // 更新之后展示
-    componentWillUpdate(){
-        console.log('componentWillUpdate执行')
-    }
-    
-    // 更新之后展示
-    componentDidUpdate(){
-        console.log('componentDidUpdate执行')
-    }
-    
-    render() {
-        console.log('render执行')
-        return (
-            <div>
-                这是一个customer组件
-                传值咯：{this.props.name}
-                <StatusPart name="受不了了"></StatusPart>
-                {/* 渲染列表 */}
-                <ul>
+  // 更新之后展示
+  componentWillUpdate() {
+    console.log("componentWillUpdate执行");
+  }
+
+  // 更新之后展示
+  componentDidUpdate() {
+    console.log("componentDidUpdate执行");
+  }
+
+  render() {
+    console.log("render执行");
+    return (
+      <div>
+        这是一个customer组件 传值咯：{this.props.name}
+        <StatusPart name="受不了了"></StatusPart>
+        {/* 渲染列表 */}
+        {/* <ul>
                     {
                         this.state.name.map((item,index)=>{
                            return <li key={index}>{item}</li>
                         })
                     }
-                </ul>
-
-                {/* 事件绑定 */}
-                <button onClick={()=>this.addOneItem()}>箭头函数=>点击添加一条记录</button>
-                <br/>
-                <button onClick={this.addOneItem1}>bind=>点击添加一条记录</button>
-                {/* anit-button */}
-                <Button type="primary" style={{width:250}}>点击添加一条记录</Button>
-            </div>
-        )
-    }
+                </ul> */}
+        {/* 用ant-design */}
+        <List renderHeader={() => "列表"}>
+          {this.state.name.map((item, index) => {
+            return <List.Item key={index}>{item}</List.Item>;
+          })}
+        </List>
+        {/* 事件绑定 */}
+        <button onClick={() => this.addOneItem()}>
+          箭头函数=>点击添加一条记录
+        </button>
+        <br />
+        <button onClick={this.addOneItem1}>bind=>点击添加一条记录</button>
+        {/* anit-button */}
+        <Button type="primary" style={{ width: 250 }} onClick={() => this.addOneItem()}>
+          点击添加一条记录
+        </Button>
+      </div>
+    );
+  }
 }
 
 // 无状态组件,传值
-function StatusPart(props){
-    const name = props.name
-    return (
-        <div>
-            这是一个无状态组件
-            name:{name}
-        </div>
-    )
+function StatusPart(props) {
+  const name = props.name;
+  return <div>这是一个无状态组件 name:{name}</div>;
 }
