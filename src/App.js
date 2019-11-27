adc37a94b86c070bfc55cf9a42162f4e207b5783
leokaka1 <leocaoxiaozhu@163.com>
2019-11-27 16:29:32 +0800

    添加了一些react的学习，并且添加了一个自定义组件，组件中有生命周期和传值的学习

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './Pages/customer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Customer name="有组件的我受不了了"></Customer>
     </div>
   );
 }
---
+++ src/Pages/customer.js
@@ -0,0 +1,78 @@
+// 有状态组件
+import React, { Component } from 'react'
+
+export default class customer extends Component {
+    constructor(props){
+        console.log('constructor执行')
+        super(props)
+        this.state={
+            name:["Leon","sisi","Giva"]
+        }
+        // 在初始化的时候绑定对应的事件
+        this.addOneItem1 = this.addOneItem1.bind(this)
+    }
+    addOneItem(){
+        this.setState({
+            name:[...this.state.name,"shasha"]
+        })
+    }
+
+    addOneItem1(){
+        this.setState({
+            name:[...this.state.name,"shasha"]
+        })
+    }
+
+    componentWillMount(){
+        console.log('componentWillMount执行')
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount执行')
+    }
+
+    // 更新之后展示
+    componentWillUpdate(){
+        console.log('componentWillUpdate执行')
+    }
+    
+    // 更新之后展示
+    componentDidUpdate(){
+        console.log('componentDidUpdate执行')
+    }
+    
+    render() {
+        console.log('render执行')
+        return (
+            <div>
+                这是一个customer组件
+                传值咯：{this.props.name}
+                <StatusPart name="受不了了"></StatusPart>
+                {/* 渲染列表 */}
+                <ul>
+                    {
+                        this.state.name.map((item,index)=>{
+                           return <li key={index}>{item}</li>
+                        })
+                    }
+                </ul>
+
+                {/* 事件绑定 */}
+                <button onClick={()=>this.addOneItem()}>箭头函数=>点击添加一条记录</button>
+                <br/>
+                <button onClick={this.addOneItem1}>bind=>点击添加一条记录</button>
+            </div>
+        )
+    }
+}
+
+// 无状态组件,传值
+function StatusPart(props){
+    const name = props.name
+    return (
+        <div>
+            这是一个无状态组件
+            name:{name}
+        </div>
+    )
+}
