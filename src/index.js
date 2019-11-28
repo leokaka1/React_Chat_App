d7a988eebc2dcd42d8b4f4b4cbdc8878049297c1
leokaka1 <leocaoxiaozhu@163.com>
2019-11-28 14:49:28 +0800

    引入了react-router-dmo,并且整改了index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
+import store from './store/store'
 import './config'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function MyApp(){
+    return (
+        <Provider store = {store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    )
+}
+
+ReactDOM.render(MyApp(), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
