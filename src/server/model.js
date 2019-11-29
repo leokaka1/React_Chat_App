ded73a59a8e8f9607b0c221efe1b76eeeb64c347
leokaka1 <leocaoxiaozhu@163.com>
2019-11-29 10:45:36 +0800

    完善了服务端的模型类，引入了body-parser来获取请求的body数据

--- src/server/model.js
+++ src/server/model.js
@@ -5,3 +5,32 @@ mongoose.connect(db_url);
 mongoose.connection.on("connected", () => {
   console.log("☆☆☆☆☆☆☆mongodb is already connected!!!☆☆☆☆☆☆☆☆");
 });
+
+const model = {
+  user: {
+    user: { type: String, require: true },
+    pwd: { type: String, require: true },
+    type: { type: String, require: true },
+    // 头像
+    avatar: { type: String },
+    // 个人简介或者职位简介
+    desc: { type: String },
+    // 职位名
+    title: { type: String },
+    // 如果是老板职位
+    company: { type: String },
+    // 薪资
+    salary: { type: String }
+  }
+};
+
+// 批量取出模型
+for (let m in model) {
+  mongoose.model(m, new mongoose.Schema(model[m]));
+}
+
+module.exports = {
+  getModel:function(name){
+    return mongoose.model(name)
+  }
+}
--- src/server/user.js
+++ src/server/user.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const Router = express.Router()
 
+const model = require('./model')
+const User = model.getModel('user')
+
+Router.get('/list',(req,res)=>{
+    User.find({},function(err,data){
+        return res.json(data)
+    })
+})
+
 // 利用Router挂载
 Router.get('/info',(req,res)=>{
     return res.send({"code":1})
